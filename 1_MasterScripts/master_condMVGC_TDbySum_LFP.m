8f856c600e3464b8159cf0a56a42a633701baaee
Adam Smoulder <asmoulder3195@gmail.com>
2017-07-26 18:46:10 -0400

    updated

--- 1_MasterScripts/master_condMVGC_TDbySum_LFP.m
+++ 1_MasterScripts/master_condMVGC_TDbySum_LFP.m
@@ -27,7 +27,7 @@
 analysisType = 'cond';              % 'cond' = conditional, 'pw' = pairwise
 monkey_date = 'bb_sc_080415';       % experiment, used for saving
 signalType = 'lfp';                 % 'lfp' or 'spikes'
-domain = 'TDBySum';                 % 'SD' = spectral, 'TD' = time domain, 'TDbySum' = spectral then sum over frequencies
+domain = 'TDBySum';                      % 'SD' = spectral, 'TD' = time domain, 'TDbySum' = spectral then sum over frequencies
 channelsToUse = [1 3; 5 7; 9 11];   % superficial ; mid ; deep - change all depending on experiment
 demeanTrialAvg = 0;                 % 0 = do not demean data by trial avg, 1 = do so
 axisLimit = 0;                      % if 0, axisLimit = maxGC (only used for subtractorExtractor)
@@ -72,7 +72,7 @@ disp('saccade, outtarg')
 inTargVal = 0;
 inTargString = getInTargString(inTargVal);
 outTarg_SaccFolderName = ['FiguresAndResults/',analysisType,'/',monkey_date,'/',domain,'/',signalType,'/IndividualCues/',cueType];
-outTarg_SaccFileString = [analysisType,'_',monkey_date,'_',domain,'_',signalType,'_',cueType,'_',inTargString,'_',demeaned];
+outTarg_SaccFileString = [analysisType,'_',monkey_date,'_',domain,'_',signalType,'_',cueType,'_',inTargString,demeaned];
 eval(['mkdir ',outTarg_SaccFolderName]);
 performMVGC;
 saveas(figure(66), [outTarg_SaccFolderName,'/',outTarg_SaccFileString]);
@@ -83,11 +83,11 @@ save([outTarg_SaccFolderName,'/',outTarg_SaccFileString]);
 % saccade, in - out
 disp('...')
 disp('saccade, in - out')
-inMinusOutTarg_SaccFolderString = [projectRoot,'FiguresAndResults/',analysisType,'/',monkey_date,'/',domain,'/',signalType,'/IndividualCues/',cueType];
-inMinusOutTarg_SaccFileString = [analysisType,'_',monkey_date,'_',domain,'_',signalType,'_',cueType,'_',inTargString,'_',demeaned];
+inMinusOutTarg_SaccFolderString = ['FiguresAndResults/',analysisType,'/',monkey_date,'/',domain,'/',signalType,'/IndividualCues/',cueType];
+inMinusOutTarg_SaccFileString = [analysisType,'_',monkey_date,'_',domain,'_',signalType,'_',cueType,'_',inTargString,demeaned];
 eval(['mkdir ',inMinusOutTarg_SaccFolderString]);
-inTargFileString = [inTarg_SaccFolderName,'/',inTarg_SaccFileString,'.mat'];
-outTargFileString = [outTarg_SaccFolderName,'/',outTarg_SaccFileString,'.mat'];
+inTargFileString = [inTarg_SaccFolderName,'/',inTarg_SaccFileString];
+outTargFileString = [outTarg_SaccFolderName,'/',outTarg_SaccFileString];
 performGCSubtraction;
 saveas(figure(66), [inMinusOutTarg_SaccFolderString,'/',inMinusOutTarg_SaccFileString]); % save figure
 clf
@@ -103,7 +103,7 @@ cueString = getCueString(cueType,signalType);
 inTargVal = 1;
 inTargString = getInTargString(inTargVal);
 inTarg_TargFolderName = ['FiguresAndResults/',analysisType,'/',monkey_date,'/',domain,'/',signalType,'/IndividualCues/',cueType];
-inTarg_TargFileString = [analysisType,'_',monkey_date,'_',domain,'_',signalType,'_',cueType,'_',inTargString,'_',demeaned];
+inTarg_TargFileString = [analysisType,'_',monkey_date,'_',domain,'_',signalType,'_',cueType,'_',inTargString,demeaned];
 eval(['mkdir ',inTarg_TargFolderName]);
 performMVGC;
 saveas(figure(66), [inTarg_TargFolderName,'/',inTarg_TargFileString]);
@@ -117,7 +117,7 @@ disp('target, outtarg')
 inTargVal = 0;
 inTargString = getInTargString(inTargVal);
 outTarg_TargFolderName = ['FiguresAndResults/',analysisType,'/',monkey_date,'/',domain,'/',signalType,'/IndividualCues/',cueType];
-outTarg_TargFileString = [analysisType,'_',monkey_date,'_',domain,'_',signalType,'_',cueType,'_',inTargString,'_',demeaned];
+outTarg_TargFileString = [analysisType,'_',monkey_date,'_',domain,'_',signalType,'_',cueType,'_',inTargString,demeaned];
 eval(['mkdir ',outTarg_TargFolderName]);
 performMVGC;
 saveas(figure(66), [outTarg_TargFolderName,'/',outTarg_TargFileString]);
@@ -128,11 +128,11 @@ save([outTarg_TargFolderName,'/',outTarg_TargFileString]);
 % target, in - out
 disp('...')
 disp('target, in - out')
-inMinusOutTarg_TargFolderString = [projectRoot,'FiguresAndResults/',analysisType,'/',monkey_date,'/',domain,'/',signalType,'/IndividualCues/',cueType];
-inMinusOutTarg_TargFileString = [analysisType,'_',monkey_date,'_',domain,'_',signalType,'_',cueType,'_',inTargString,'_',demeaned];
+inMinusOutTarg_TargFolderString = ['FiguresAndResults/',analysisType,'/',monkey_date,'/',domain,'/',signalType,'/IndividualCues/',cueType];
+inMinusOutTarg_TargFileString = [analysisType,'_',monkey_date,'_',domain,'_',signalType,'_',cueType,'_',inTargString,demeaned];
 eval(['mkdir ',inMinusOutTarg_TargFolderString]);
-inTargFileString = [inTarg_TargFolderName,'/',inTarg_TargFileString,'.mat'];
-outTargFileString = [outTarg_TargFolderName,'/',outTarg_TargFileString,'.mat'];
+inTargFileString = [inTarg_TargFolderName,'/',inTarg_TargFileString];
+outTargFileString = [outTarg_TargFolderName,'/',outTarg_TargFileString];
 performGCSubtraction;
 saveas(figure(66), [inMinusOutTarg_TargFolderString,'/',inMinusOutTarg_TargFileString]);
 clf 
@@ -147,48 +147,52 @@ disp('Plot for figures')
 switch domain
     case 'SD'
         axisLimit = 1;
+        axisLimitString = '1';
     case 'TD'
         axisLimit = 0.2;
+        axisLimitString = '0p2';
     case 'TDBySum'
         axisLimit = 1500;
+        axisLimitString = '1500';
     otherwise
         axisLimit = 0;
+        axisLimitString = '0';
 end
-firstAxisLimitFolderString = [projectRoot,'FiguresAndResults/',analysisType,'/',monkey_date,'/',domain,'/',signalType,'/CombinedCues/',axisLimit];
+firstAxisLimitFolderString = ['FiguresAndResults/',analysisType,'/',monkey_date,'/',domain,'/',signalType,'/CombinedCues/',axisLimitString];
 eval(['mkdir ',firstAxisLimitFolderString]);
 
 % combined figures, intarg, axisLimit = 1
 disp('...')
-disp(['intarg, max axis GC = ', axisLimit])
+disp(['intarg, max axis GC = ', axisLimitString])
 saccFileString = inTarg_SaccFileString; % Or whatever the name is in the script
 targFileString = inTarg_TargFileString;
 performCombinedPlot;
 inTargString = getInTargString(1);
-fileName = [analysisType,'_',monkey_date,'_',domain,'_',signalType,'_',inTargString,'_',demeaned,axisLimit];
+fileName = [analysisType,'_',monkey_date,'_',domain,'_',signalType,'_',inTargString,'_',demeaned,axisLimitString];
 saveas(figure(66), [firstAxisLimitFolderString,'/',fileName]);
 clf
 close all
 
 % combined figures, outtarg, axisLimit = 1
 disp('...')
-disp(['outtarg, max axis GC = ', axisLimit])
+disp(['outtarg, max axis GC = ', axisLimitString])
 saccFileString = outTarg_SaccFileString;
 targFileString = outTarg_TargFileString;
 performCombinedPlot;
 inTargString = getInTargString(0);
-fileName = [analysisType,'_',monkey_date,'_',domain,'_',signalType,'_',inTargString,'_',demeaned,axisLimit];
+fileName = [analysisType,'_',monkey_date,'_',domain,'_',signalType,'_',inTargString,'_',demeaned,axisLimitString];
 saveas(figure(66), [firstAxisLimitFolderString,'/',fileName]);
 clf
 close all
 
 % combined figures, in-out, axisLimit = 1
 disp('...')
-disp(['in-out, max axis GC = ', axisLimit])
+disp(['in-out, max axis GC = ', axisLimitString])
 saccFileString = inMinusOutTarg_SaccFileString;
 targFileString = inMinusOutTarg_TargFileString;
 performCombinedPlot;
 inTargString = getInTargString(42);
-fileName = [analysisType,'_',monkey_date,'_',domain,'_',signalType,'_',inTargString,'_',demeaned,axisLimit];
+fileName = [analysisType,'_',monkey_date,'_',domain,'_',signalType,'_',inTargString,'_',demeaned,axisLimitString];
 saveas(figure(66), [firstAxisLimitFolderString,'/',fileName]);
 clf 
 close all
@@ -199,48 +203,52 @@ close all
 switch domain
     case 'SD'
         axisLimit = 0.5;
+        axisLimitString = '0p5';
     case 'TD'
         axisLimit = 0.1;
+        axisLimitString = '0p1';
     case 'TDBySum'
         axisLimit = 700;
+        axisLimitString = '700';
     otherwise
         axisLimit = 0;
+        axisLimitString = '0';
 end
-endsecondAxisLimitFolderString = [projectRoot,'FiguresAndResults/',analysisType,'/',monkey_date,'/',domain,'/',signalType,'/CombinedCues/',axisLimit];
+secondAxisLimitFolderString = ['FiguresAndResults/',analysisType,'/',monkey_date,'/',domain,'/',signalType,'/CombinedCues/',axisLimitString];
 eval(['mkdir ',secondAxisLimitFolderString]);
 
 % combined figures, intarg, axisLimit = 0.5
 disp('...')
-disp(['intarg, max axis GC = ', axisLimit])
+disp(['intarg, max axis GC = ', axisLimitString])
 saccFileString = inTarg_SaccFileString;
 targFileString = inTarg_TargFileString;
 performCombinedPlot;
 inTargString = getInTargString(1);
-fileName = [analysisType,'_',monkey_date,'_',domain,'_',signalType,'_',inTargString,'_',demeaned,axisLimit];
+fileName = [analysisType,'_',monkey_date,'_',domain,'_',signalType,'_',inTargString,'_',demeaned,axisLimitString];
 saveas(figure(66), [secondAxisLimitFolderString,'/',fileName]);
 clf
 close all
 
 % combined figures, outtarg, axisLimit = 0.5
 disp('...')
-disp(['outtarg, max axis GC = ', axisLimit])
+disp(['outtarg, max axis GC = ', axisLimitString])
 saccFileString = outTarg_SaccFileString;
 targFileString = outTarg_TargFileString;
 performCombinedPlot;
 inTargString = getInTargString(0);
-fileName = [analysisType,'_',monkey_date,'_',domain,'_',signalType,'_',inTargString,'_',demeaned,axisLimit];
+fileName = [analysisType,'_',monkey_date,'_',domain,'_',signalType,'_',inTargString,'_',demeaned,axisLimitString];
 saveas(figure(66), [secondAxisLimitFolderString,'/',fileName]);
 clf
 close all
 
 % combined figures, in-out, axisLimit = 0.5
 disp('...')
-disp(['in-out, max axis GC = ', axisLimit])
+disp(['in-out, max axis GC = ', axisLimitString])
 saccFileString = inMinusOutTarg_SaccFileString;
 targFileString = inMinusOutTarg_TargFileString;
 performCombinedPlot;
 inTargString = getInTargString(42);
-fileName = [analysisType,'_',monkey_date,'_',domain,'_',signalType,'_',inTargString,'_',demeaned,axisLimit];
+fileName = [analysisType,'_',monkey_date,'_',domain,'_',signalType,'_',inTargString,'_',demeaned,axisLimitString];
 saveas(figure(66), [secondAxisLimitFolderString,'/',fileName]);
 clf
 close all
