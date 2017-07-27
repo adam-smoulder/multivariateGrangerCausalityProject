41164f16255a7e3e558b255b528a1dc2b1b5f5b8
Adam Smoulder <asmoulder3195@gmail.com>
2017-07-27 10:43:57 -0400

    updated for condMVGC

--- 3_SubScripts/performMVGC.m
+++ 3_SubScripts/performMVGC.m
@@ -1,12 +1,10 @@
 % performs MVGC based on domain
 
-if  strcmp(domain, 'SD')
-    condMVGC_SD; % perform spectral-domain MVGC
-elseif strcmp(domain, 'TD')
-    condMVGC_TD; % perform time-domain MVGC
-elseif strcmp(domain,'TDBySum')
-    condMVGC_TDBySum; % perform spectral-domain MVGC then sum over frequencies
+if  strcmp(analysisType, 'cond')
+    condMVGC;
+% elseif strcmp(analysisType, 'pw')
+%     pwMVGC;
 else
-    ME = MException('MyComponent:noSuchVariable','%s is not a domain type (SD, TD, TDBySum)',domain);
+    ME = MException('MyComponent:noSuchVariable','%s analysis is not implemented',analysisType);
         throw(ME)
 end
